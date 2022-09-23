d5ed9b003d0fe2e11ece2d5406056acd9eea2c1d
NikkiMerena <alderman.nikki1022@gmail.com>
2022-09-23 08:07:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,14 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * main - main.h
+ * Description: header file containting declarations
+ * for all functions in 0x02-functions_nested_loops
+ */
+#include <stdio>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alpabet_x10(void);
@@ -10,3 +21,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int x, int y);
 void print_to_98(int n);
+#endif
