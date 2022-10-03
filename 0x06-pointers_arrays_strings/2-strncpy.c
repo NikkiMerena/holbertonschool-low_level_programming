d049a53768dafadd9d4f85d0be82a9d36c949d8f
NikkiMerena <alderman.nikki1022@gmail.com>
2022-10-03 08:51:41 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strncpy -copy a string
+ * @dest: we copy to
+ * @src: we copy from
+ * @n: we copy by
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *begin = dest;
+
+	for (; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (begin);
+}
