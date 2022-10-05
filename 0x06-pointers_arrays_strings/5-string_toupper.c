5132a78cf633c6ffdf556c46be35da2f1d2218d5
NikkiMerena <alderman.nikki1022@gmail.com>
2022-10-04 19:44:34 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase characters for uppercase
+ * @a: String
+ *
+ * Return: String as a pointer
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (*(a + i))
+	{
+		if (*(a + i) >= 'a' && *a <= 'z')
+			*(a + i) -= 32;
+	i++;
+	}
+	return (a);
+}
