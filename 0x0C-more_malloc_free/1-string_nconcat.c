affe094bd8f60b9dab72a14e952010092b80a6f1
NikkiMerena <alderman.nikki1022@gmail.com>
2022-10-18 18:57:50 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,29 +11,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-
-
-	unsigned int x1, x2, i, j;
-	char *s;
+	unsigned int i = 0, j = 0, k = 0, l = 0, m = 0, q = 0;
+	char *a;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (x1 = 0; s1[x1] != '\0'; x1++)
-	;
-	for (x2 = 0; s2[x2] != '\0'; x2++)
-	;
-	if (n >= x2)
-		n = x2;
-	s = malloc(sizeof(char) * (x1 + n + 1));
-	if (s == NULL)
+	while (*(s1 + i))
+		i++;
+	while (*(s2 + j))
+		j++;
+
+	if (n >= j)
+		n = j;
+
+	a = malloc(sizeof(char) * (i + n + 1));
+
+	if (a == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		s[i] = s1[i];
-	for (j = x1; j < x1 + n; j++)
-		s[j] = s2[j - x1];
-	s[j] = '\0';
-	return (s);
+	for (k = 0; k <= i; k++)
+	{
+		*(a + k) = *(s1 + l);
+		l++;
+	}
+	for (q = 0; q < n; q++)
+	{
+		*(a + q + i) = *(s2 + m);
+		m++;
+	}
+		*(a + q + i) = '\0';
+	return (a);
 }
