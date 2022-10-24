6d0e44d2ce1c92c3be5242a73dc8b7c2e6555089
NikkiMerena <alderman.nikki1022@gmail.com>
2022-10-24 10:12:42 -0700

    1-create_dynamic_lib.sh

---
+++ 0x0A-dynamic_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - upper case characters
+ *
+ * @c: This has letters
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x0A-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - string
+ * @s: strings input
+ * Return: returning
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*(s + length) != '\0')
+length++;
+return (length);
+}
